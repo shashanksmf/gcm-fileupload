8055313821c951db1e4e4c42b9dcf539124fe6fd
shashank <shashanksmf@outlook.com>
2018-04-18 21:26:59 +0530

    process port addded

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 
 var parser = require('./firebase/parseFiles.js')
 app.get('/', parser.file);
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
