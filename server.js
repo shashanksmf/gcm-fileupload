ac7091df092961ef1d1ec480042b2e569e75b3da
shashank <shashanksmf@outlook.com>
2018-04-18 21:45:04 +0530

    route added

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express();
 
 var parser = require('./firebase/parseFiles.js')
-app.get('/', parser.file);
+app.get('/upload', parser.file);
+app.get('/', (req,res)=>{
+  res.send("hello world");
+});
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
